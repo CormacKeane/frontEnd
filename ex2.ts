3c9ae7575c041610006e8f5e80708e729d247763
CormacKeane <G00429254@atu.ie>
2026-02-09 10:50:38 +0000

    added comments
    Implemented task management functions including adding, listing, and deleting tasks.

--- ex2.ts
+++ ex2.ts
@@ -1,3 +1,4 @@
+//interface implementation with all functions included
 interface myInter {
     myTasks: string[];
     addTasks(task: string): number;
@@ -10,21 +11,24 @@ interface myInter {
 class Tasks implements myInter {
     constructor() { }
 
+    //array init
     myTasks: string[] = [];
 
+    //Add task function to push on to array
     addTasks(task: string): number {
         this.myTasks.push(task);
         console.log(task + " has been added to my taks list.");
         return this.myTasks.length;
     }
 
-
+    //for loop to list all task in the array
     listAllItems(): void {
         for (let i = 0; i < this.myTasks.length; i++) {
             console.log("Task: " + this.myTasks[i] + " is in my task list.");
         }
     }
 
+    //function to splice array to delete specific index
     deleteTask(Task: string): number {
         let index: number = this.myTasks.indexOf(Task);
         if (index === -1) {
@@ -38,7 +42,7 @@ class Tasks implements myInter {
 
 }
 
-
+//creating instance of interface and running test commands
 let myToDo = new Tasks();
 myToDo.addTasks("Eat");
 myToDo.addTasks("Sleep");
